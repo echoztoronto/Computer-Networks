bbc9d26a7ae1a9ad3fa454c58dc4be76147179af
Yuwen <echo.zheng@mail.utoronto.ca>
2020-10-06 16:14:47 -0400

    server and part of deliver

--- lab1/deliver.c
+++ lab1/deliver.c
@@ -1,5 +1,11 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 
 int main(int argc, char *argv[])
@@ -9,5 +15,59 @@ int main(int argc, char *argv[])
         exit(0);
     }
     
+    int sockfd, address, port;
+    const int BUF_SIZE = 50;
+    
+    address = atoi(argv[1]);
+    port = atoi(argv[2]);
+    
+    //creating socket
+    sockfd = socket(PF_INET, SOCK_DGRAM, 0);
+    if(sockfd == -1) {
+        perror("Error: creating socket\n");
+        exit(1);
+    };
+    
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    if(inet_aton(argv[1], &server_addr.sin_addr) == 0) {
+        perror("Error: converting IP address\n");
+        exit(1);
+    }
+    
+    printf("Enter the message: ftp <file name>\n");
+    
+    char buf[BUF_SIZE], fname[BUF_SIZE];
+    bzero(buf, BUF_SIZE);
+    bzero(fname, BUF_SIZE);
+    
+    fgets(buf, BUF_SIZE-1, stdin);
+    
+    if(buf[0] != 'f' || buf[1] != 't' || buf[2] != 'p' || buf[3] != ' ') {
+        perror("Invalid message, usage: ftp <file name>\n");
+        exit(1);
+    }        
+        
+    char *token;
+    char delim[] = " ";
+    token = strtok(buf, delim);
+    token = strtok(NULL, delim);
+    strncpy(fname, token, sizeof token);
+    
+    //check file exists
+    if(access(fname, F_OK) == -1) {
+        perror("File doesn't exist\n");
+        exit(1);
+    }
+    
+    //TODO: send message "ftp" to server:  sendto()
+    
+    
+    //TODO: receive message from server:   recvfrom()
+    //"yes" -> print "A file transfer can start"
+    //"no" -> exit
+    
+    close(sockfd);
     return 0;
 }
--- lab1/server.c
+++ lab1/server.c
@@ -1,10 +1,10 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-
-
+#include <unistd.h>
 
 
 int main(int argc, char *argv[])
@@ -14,15 +14,58 @@ int main(int argc, char *argv[])
         exit(0);
     }
     
-    int sockfd, port;
-    struct sockaddr_in server_addr, client_addr;
+    int sockfd, port, b, byte_count, client_size, s;
+    const int BUF_SIZE = 50;
     
     port = atoi(argv[1]);
-    sockfd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (sockfd == -1) {
+    
+    //creating socket
+    sockfd = socket(PF_INET, SOCK_DGRAM, 0);
+    if(sockfd == -1) {
         perror("Error: creating socket\n");
         exit(1);
     };
     
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    
+    //binding to socket
+    b = bind(sockfd, (struct sockaddr*)&server_addr, sizeof server_addr);
+    if(b == -1) {
+        perror("Error: binding\n");
+        exit(1);
+    }
+    
+    char buf[BUF_SIZE];
+    bzero(buf,BUF_SIZE);
+    
+    struct sockaddr_in client_addr;
+    client_size = sizeof client_addr;
+    
+    //receiving from client 
+    byte_count = recvfrom(sockfd, buf, BUF_SIZE, 0, (struct sockaddr *)&client_addr, &client_size);
+    if(byte_count == -1) {
+        perror("Error: receiving\n");
+        exit(1);
+    }
+    
+    //checking "ftp" then send "yes" or "no"
+    if(strcmp(buf, "ftp") == 0) {
+        s = sendto(sockfd, "yes", strlen("yes")+1, 0, (struct sockaddr *)&client_addr, client_size);
+        if(s == -1) {
+            perror("Error: sending 'yes'\n");
+            exit(1);
+        }
+    } else {
+        s = sendto(sockfd, "no", strlen("no")+1, 0, (struct sockaddr *)&client_addr, client_size);
+        if(s == -1) {
+            perror("Error: sending 'no'\n");
+            exit(1);
+        }
+    }
+    
+    close(sockfd);
     return 0;
 }
