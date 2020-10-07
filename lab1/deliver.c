f8ab400af582745f2f9f5435637ee501ec68f09f
Ryan Valdez <ryantvaldez@Ryans-MacBook-Pro.local>
2020-10-07 02:15:41 -0400

    Finished up deliver.c and added comments. Client/Server connection seems to be working properly. Now just have to test on UG machines.

--- lab1/deliver.c
+++ lab1/deliver.c
@@ -10,64 +10,101 @@
 
 int main(int argc, char *argv[])
 {
+    // Ensure the appropriate number of args were included in the call
     if(argc != 3) {
-        perror("Usage: deliver <server address> <server port number>\n");
+        perror("Incorrect Usage");
+	printf("Call should be of form: deliver <server address> <server port number>\n Exiting...\n");
         exit(0);
     }
     
-    int sockfd, address, port;
+    // Initialize our variables
+    int sockfd, address, port, sent_count, received_count;
+    socklen_t sockaddr_size;
     const int BUF_SIZE = 50;
     
+    // Convert string arguments to ints
     address = atoi(argv[1]);
     port = atoi(argv[2]);
-    
-    //creating socket
+
+    // Create the UDP socket
     sockfd = socket(PF_INET, SOCK_DGRAM, 0);
     if(sockfd == -1) {
-        perror("Error: creating socket\n");
+        perror("Error creating socket");
+	printf("Exiting...\n");
         exit(1);
-    };
+    }
     
+    // Gather socket address and port info from args, then store in server_addr
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
     if(inet_aton(argv[1], &server_addr.sin_addr) == 0) {
-        perror("Error: converting IP address\n");
-        exit(1);
+        perror("Error converting IP address");
+        printf("Exiting...\n");
+	exit(1);
     }
     
+    sockaddr_size = sizeof(server_addr);
+
     printf("Enter the message: ftp <file name>\n");
     
     char buf[BUF_SIZE], fname[BUF_SIZE];
     bzero(buf, BUF_SIZE);
     bzero(fname, BUF_SIZE);
     
+    // Get input from user
     fgets(buf, BUF_SIZE-1, stdin);
     
+    // If user input begins with anything other than 'ftp ', throw and error and exit
     if(buf[0] != 'f' || buf[1] != 't' || buf[2] != 'p' || buf[3] != ' ') {
-        perror("Invalid message, usage: ftp <file name>\n");
+        perror("Invalid message");
+	printf("Input should be of form: ftp <file name>\n Exiting...\n");
         exit(1);
     }        
         
+    // Split the user input at the space between 'ftp' and the file name, remove the trailing \n, then copy the file name into fname
     char *token;
     char delim[] = " ";
     token = strtok(buf, delim);
     token = strtok(NULL, delim);
-    strncpy(fname, token, sizeof token);
+    token = strtok(token, "\n");
+    strncpy(fname, token, strlen(token) );
     
-    //check file exists
+    // Check if the file exists
     if(access(fname, F_OK) == -1) {
-        perror("File doesn't exist\n");
+        perror("Error finding file");
+	printf("Exiting...\n");
         exit(1);
     }
     
-    //TODO: send message "ftp" to server:  sendto()
+    // Reset the buffer to empty
+    bzero(buf, BUF_SIZE);
     
+    // Once the file is confirmed to exist, senf 'ftp' to the server
+    sent_count = sendto(sockfd, "ftp", sizeof("ftp"), 0, (struct sockaddr *)&server_addr, sockaddr_size );    
+    if(sent_count == -1){
+	perror("Error sending 'ftp'");
+	printf("Exiting...\n");
+	exit(1);
+    }
     
-    //TODO: receive message from server:   recvfrom()
-    //"yes" -> print "A file transfer can start"
-    //"no" -> exit
+    // Check the socket for a reply from the server
+    // A 'yes' means a file transfer can start. Anything else results in an exit.
+    received_count = recvfrom(sockfd, buf, BUF_SIZE, 0, (struct sockaddr *)&server_addr, &sockaddr_size );
+    if(received_count == -1){
+	perror("Error receiving message");
+        printf("Exiting...\n");
+	exit(1);
+    }
     
+    if( strcmp(buf, "yes") == 0 ){
+	printf("A file transfer can start.\n");
+    } else{
+        printf("Did not receive 'yes'\n Exiting...\n");
+	exit(1);
+    }	
+
+    // Close the socket
     close(sockfd);
     return 0;
 }
--- lab1/server.c
+++ lab1/server.c
@@ -9,32 +9,40 @@
 
 int main(int argc, char *argv[])
 {
+    // Ensure the appropriate number of args were included in the call	
     if(argc != 2) {
-        perror("Usage: server <UDP listen port>\n");
+	perror("Incorrect Usage");
+        printf("Correct usage: server <UDP listen port>\n Exiting...\n");
         exit(0);
     }
     
-    int sockfd, port, b, byte_count, client_size, s;
+    // Initialize our variables
+    int sockfd, port, b, byte_count, s;
+    socklen_t client_size;
     const int BUF_SIZE = 50;
     
+    // Convert the port value from string to int
     port = atoi(argv[1]);
     
-    //creating socket
+    // Create the UDP socket
     sockfd = socket(PF_INET, SOCK_DGRAM, 0);
     if(sockfd == -1) {
-        perror("Error: creating socket\n");
+        perror("Error creating socket");
+	printf("Exiting...\n");
         exit(1);
     };
     
+    // Store the socket IP address (local) and port in server_addr
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     
-    //binding to socket
+    // Bind to the socket in order to listen for connections
     b = bind(sockfd, (struct sockaddr*)&server_addr, sizeof server_addr);
     if(b == -1) {
-        perror("Error: binding\n");
+        perror("Error binding");
+	printf("Exiting...\n");
         exit(1);
     }
     
@@ -44,28 +52,34 @@ int main(int argc, char *argv[])
     struct sockaddr_in client_addr;
     client_size = sizeof client_addr;
     
-    //receiving from client 
+    // Receive message from client by listening on correct port
+    // Store message received into buf 
     byte_count = recvfrom(sockfd, buf, BUF_SIZE, 0, (struct sockaddr *)&client_addr, &client_size);
     if(byte_count == -1) {
-        perror("Error: receiving\n");
+        perror("Error receiving");
+	printf("Exiting...\n");
         exit(1);
     }
     
-    //checking "ftp" then send "yes" or "no"
+    // If the message received from the client is 'ftp', reply to the client with 'yes'
+    // If the message received is anything else, reply to the client with 'no'
     if(strcmp(buf, "ftp") == 0) {
         s = sendto(sockfd, "yes", strlen("yes")+1, 0, (struct sockaddr *)&client_addr, client_size);
         if(s == -1) {
-            perror("Error: sending 'yes'\n");
+            perror("Error sending 'yes'");
+	    printf("Exiting...\n");
             exit(1);
         }
     } else {
         s = sendto(sockfd, "no", strlen("no")+1, 0, (struct sockaddr *)&client_addr, client_size);
         if(s == -1) {
-            perror("Error: sending 'no'\n");
-            exit(1);
+            perror("Error sending 'no'\n");
+            printf("Exiting...\n");
+	    exit(1);
         }
     }
     
+    // Close the socket
     close(sockfd);
     return 0;
 }
